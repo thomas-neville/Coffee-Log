237ddc937b15466a00e8e9fe3ea9b489069bafd4
thomas-neville <thomasneville01@gmail.com>
2023-03-20 11:53:59 -0700

    added props and constructor to brew model

---
+++ CoffeeLogAppv1/Models/Brew.cs
@@ -0,0 +1,20 @@
+﻿namespace CoffeeLogAppv1.Models
+{
+    public class Brew
+    {
+        // props for brew logs
+        public int Id { get; set; }
+        public string CoffeeName { get; set; }
+        public double BrewTime { get; set; }
+        public double DoseGrams { get; set; }
+        public int GrindSetting { get; set; }
+        public int WaterTemp { get; set; }
+        public string Notes { get; set; }
+
+        // constructor
+        public Brew()
+        {
+            
+        }
+    }
+}
