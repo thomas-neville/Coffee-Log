d17ad9801bbbad508306123fff525fb5ca8c87d7
thomas-neville <thomasneville01@gmail.com>
2023-03-20 14:41:16 -0700

    msswl db and configured, brew table added, and nav link added

--- CoffeeLogAppv1/Data/Migrations/20230320211342_initialbuild.Designer.cs
+++ CoffeeLogAppv1/Data/Migrations/20230320211342_initialbuild.Designer.cs
@@ -4,6 +4,7 @@ using CoffeeLogAppv1.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CoffeeLogAppv1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230320211342_initialbuild")]
+    partial class initialbuild
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CoffeeLogAppv1/Data/Migrations/20230320211342_initialbuild.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeLogAppv1.Data.Migrations
+{
+    public partial class initialbuild : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brew",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CoffeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BrewTime = table.Column<double>(type: "float", nullable: false),
+                    DoseGrams = table.Column<double>(type: "float", nullable: false),
+                    GrindSetting = table.Column<int>(type: "int", nullable: false),
+                    WaterTemp = table.Column<int>(type: "int", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brew", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Brew");
+        }
+    }
+}
